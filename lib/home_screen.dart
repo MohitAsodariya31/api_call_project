a3c23e56033e1e7af94b9fce209b83aa4eb0c28f
mohitasoda <mohitasodariya07@gmail.com>
2023-03-16 17:46:24 +0530

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:api_call/data_screen.dart';
+import 'package:api_call/model/post_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  LocationModel? locationModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getPostAPI();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+        centerTitle: true,
+      ),
+      body: locationModel == null
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              itemCount: locationModel!.results!.length,
+              separatorBuilder: (context, index) => const SizedBox(height: 15),
+              itemBuilder: (context, index) => ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DataScreen(
+                        info: locationModel!.info,
+                        results: locationModel!.results![index],
+                      ),
+                    ),
+                  );
+                },
+                tileColor: Colors.green,
+                title: Text(
+                  'LocationName :  ${locationModel!.results![index].name}',
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                subtitle: Text(
+                  'URLName : ${locationModel!.results![index].url}',
+                  style: const TextStyle(fontSize: 14.5, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+    );
+  }
+
+  getPostAPI() async {
+    var client = http.Client();
+    try {
+      Response response = await client.get(Uri.parse("https://rickandmortyapi.com/api/location"));
+      if (response.statusCode == 200) {
+        locationModel = LocationModel.fromJson(jsonDecode(response.body));
+        debugPrint("characterModal -------------->>> ${locationModel!.toJson()}");
+        setState(() {});
+      } else {
+        debugPrint("Status Code -------------->>> ${response.statusCode}");
+      }
+    } finally {
+      client.close();
+    }
+  }
+}
